a4556077a5dae1ebee09e8bcbf8e7e0158651985
sohrab <sohrab.ahmadpur@gmail.com>
2024-06-08 14:33:48 +0400

    added language support

--- Readme.txt
+++ Readme.txt
@@ -1,58 +1,77 @@
+Startseite
+
+Kontakt
+
+Datenschutzrichtlinie
+
+Unsere Aufgabe
+
+DE
+
+Wir bieten Expertenunterstützung bei Beschäftigungs- und Visafragen, um Ihre legale Einreise nach Deutschland zu erleichtern
+
+Über Uns
+
+KH Recruiting ist ein Unternehmen, das sich auf die Vermittlung von Fachkräften aus dem Ausland für offene Stellen auf dem deutschen Arbeitsmarkt spezialisiert hat. Es bietet Ausländern Orientierung bei der Erwerbstätigkeit und Ankunft in Deutschland. Neben der Bereitstellung von Unterstützung bei der Arbeitssuche und Visa für alle, die nach Deutschland kommen möchten, helfen wir auch Personen, die an Ausbildungs-/Weiterbildungsprogrammen teilnehmen oder eine Ausbildung absolvieren möchten, bei Fragen zu Visa und Schulen.
 
 
 
+Unsere Mission konzentriert sich darauf, den Prozess für diejenigen zu erleichtern, die sich in Deutschland niederlassen möchten, und sie mit passenden Arbeitsmöglichkeiten zu verbinden. Unser erfahrenes Team bietet Beratung und Unterstützung in jedem Schritt, angefangen von der Arbeitssuche bis hin zu Visa-Anträgen, Bildungsmöglichkeiten und Siedlungsphasen.
 
+Bei KH Recruiting stehen wir jedem Einzelnen zur Seite, um die erforderliche Unterstützung für eine erfolgreiche Karriere oder Bildung in Deutschland zu bieten.
 
-Müştəri Şərhləri
+Kunden-Kommentare
 
-Çox güvənilir şirkətdir. Sürətli və effektiv xidmət aldım.
+Ein sehr zuverlässiges Unternehmen. Ich habe einen schnellen und effektiven Service erhalten.
 
-KH Recruiting ilə Almaniyaya gəlmək çox asan idi. Təşəkkürlər!
+Mit KH Recruiting war es sehr einfach, nach Deutschland zu kommen. Danke!
 
+Name
 
-AZ
+Email*
 
-Almaniyaya gəliş prosesinizi asanlaşdırmaq üçün biznes və viza məsələləri üzrə ekspert dəstəyi
+Message*
 
-HAQQIMIZDA
+Einreichen
 
-KH Recruiting Almaniyada iş və işçi tapmaqda ixtisaslaşan, əcnəbilərin Almaniyaya gəlməsinə rəhbərlik edən bir şirkətdir. Almaniyaya gəlmək istəyən hər kəsə iş və viza dəstəyi verməklə yanaşı, Ausbildung proqramlarında iştirak etmək və ya təhsil almaq istəyənlərə də viza və məktəb dəstəyi veririk.
+Kontaktinformationen
 
+Kontakte
 
-Azərbaycanca: "Azərbaycanca",
-Русский: "Русский",
-Deutsch: "Deutsch",
-home: "Ana səhifə",
-about: "Haqqımızda",
-services: "Xidmətlərimiz",
-portfolio: "Portfolio",
-team: "Komandamız",
-contact: "Əlaqə",
-hero1: "Almaniyaya gəliş prosesinizi asanlaşdırmaq üçün",
-hero2: "Biznes və Viza",
-hero3: "məsələləri üzrə ekspert dəstəyi",
-get_started_btn: "Ətraflı",
-services_btn: "Xidmətlərimiz",
-about_text: "KH Recruiting - biz hər bir insana Almaniyada uğurlu karyera və ya təhsil həyatı üçün lazım olan dəstəyi təmin etmək üçün buradayıq.",
-services_text: "KH Recruiting Almaniyada iş və işçi tapmaqda ixtisaslaşan, əcnəbilərin Almaniyaya gəlməsinə rəhbərlik edən bir şirkətdir. Almaniyaya gəlmək istəyən hər kəsə iş və viza dəstəyi verməklə yanaşı, Ausbildung proqramlarında iştirak etmək və ya təhsil almaq istəyənlərə də viza və məktəb dəstəyi veririk."
+info@recruitingkh.com
 
+Sozial
 
++49 1521 0453565
 
+Startseite
 
+Kontakt
 
-info@haundfa-nord.com
-Schwarzenbergstr 24c
-21073  Hamburg
-[15:36, 28/05/2024] +49 172 8012554: +49 1577 4446745
+Datenschutzrichtlinie
 
+Unsere Aufgabe
 
-ervices_text: "KH Recruiting Almaniyada iş və işçi tapmaqda ixtisaslaşan, əcnəbilərin Almaniyaya gəlməsinə rəhbərlik edən bir şirkətdir. Almaniyaya gəlmək istəyən hər kəsə iş və viza dəstəyi verməklə yanaşı, Ausbildung proqramlarında iştirak etmək və ya təhsil almaq istəyənlərə də viza və məktəb dəstəyi veririk."
+DE
 
-edu_visa: "Təhsil vizası",
-edu_visa_text: "Təhsiliniz ilə bağlı viza almağa tam dəstək oluruq",
-work_visa: "İş vizası",
-work_visa_text: "Almaniyada işləmək arzusundasınzsa, Biz həm viza həmdə işlə bağlı dəstək ola bilirik",
-aus: "Ausbildung proqramı",
-aus_text:"Yeni karyeranızı Almaniyada qurmağı düşünürsüzə, Bizə müraciət edin",
+Unsere Aufgabe
+
+KH RECRUİTİNG ist ein Team, das sich auf Arbeitsvisa und Bildungsdienstleistungen in Deutschland spezialisiert hat. Unsere Mission ist es, ausländischen Staatsangehörigen zu helfen, in Deutschland eine Karriere aufzubauen und sie bei der Jobsuche zu unterstützen. Sowohl in der Geschäftswelt als auch im Bildungssektor bieten wir verschiedene Ressourcen und Dienstleistungen an, um Kandidaten bei der Verwirklichung ihrer Ziele zu unterstützen.
+
+Unser Hauptziel ist es, die Bedürfnisse ausländischer Jobsuchender zu verstehen und ihnen passende Stellenangebote anzubieten. Wir verfügen über ein breites Netzwerk und Branchenkenntnisse. Dadurch begleiten wir Sie von der Erstellung des Lebenslaufs über Bewerbungsgespräche bis zur erfolgreichen Platzierung durch jeden Schritt. Gleichzeitig streben wir danach, Kandidaten bei den erforderlichen Verfahren für den Erhalt von Arbeitsvisa zu unterstützen, damit Sie die besten Chancen auf eine Beschäftigung in Deutschland wahrnehmen können.
+
+Wir übernehmen auch eine führende Rolle in der Ausbildung und Weiterbildung. Wir bieten ein umfangreiches Schulungsprogramm, das von unseren Experten unterstützt wird, um Kandidaten bei der Entwicklung ihrer beruflichen Fähigkeiten und die Fortbildung und berufliche Entwicklung zu fördern. Diese Programme zielen darauf ab, die Entwicklung in der Branche zu verfolgen und Kandidaten dabei zu helfen, in Deutschland eine erfolgreiche Karriere zu starten.
+
+Unser Ziel bei KH RECRUİTİNG ist es, ausländischen Staatsangehörigen das Gefühl von Sicherheit bei der Jobsuche und Ausbildung in Deutschland zu vermitteln und ihnen dabei zu helfen, eine erfolgreiche Zukunft aufzubauen. Jeder einzelne hat bei uns das Potenzial für eine erfolgreiche Karriere, und wir streben danach, individuelle Lösungen anzubieten, um den Erfolg unserer Kandidaten zu fördern.
+
+Für weitere Informationen und Kontaktaufnahme erreichen Sie uns unter info@recruitingkh.com.
+
+Kontakte
+
+info@recruitingkh.com
+
+Sozial
+
++49 1521 0453565
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,8 @@ const translations = {
         phone: "Telefon:",
         email: "E-ünvan",
         links: "Keçidlər",
+        testimonal1: "Çox güvənilir şirkətdir. Sürətli və effektiv xidmət aldım.",
+        testimonal2: "Ha&Fa Nord ilə Almaniyaya gəlmək çox asan idi. Təşəkkürlər!",
         terms: "Xidmət Şərtləri",
         privacy: "Məxfilik siyasəti",
         edu_visa: "Təhsil vizası",
@@ -41,25 +43,68 @@ const translations = {
     Ru: {
         Azərbaycanca: "Azərbaycanca",
         Русский: "Русский",
-        Deutsch: "Немецкий",
-        about: "О нас",
+        Deutsch: "Deutsch",
+        home: "Главная страница",
+        about: "О Нас",
         services: "Услуги",
-        contact: "Контакты",
         portfolio: "Портфолио",
-        text1: "Ha&Fa Nord",
-        text2: "Мы предлагаем экспертную помощь в вопросах трудоустройства и получения визы для облегчения вашего легального въезда в Германию",
-        text3: "Наша миссия заключается в облегчении процесса для тех, кто хочет освоиться в Германии, и связывании их с подходящими возможностями занятости. Наш опытный коллектив предоставляет консультации и поддержку на каждом этапе, начиная от поиска работы и заканчивая оформлением виз, возможностями получения образования и этапами освоения.",
-        button1: "Узнать больше",
-        description: "Esta es una plantilla de sitio web multilingüe."
+        team: "Команда",
+        contact: "Контакты",
+        testimonals: "Комментарии клиентов",
+        hero1: "Экспертная поддержка по вопросам",  
+        hero2: "Бизнеса и Виз",
+        hero3: "для облегчения процесса вашего прибытия в Германию",
+        get_started_btn: "Подробнее",
+        services_btn: "Услуги",
+        phone: "Телефон:",
+        email: "Е-почта",
+        links: "Ссылки",
+        testimonal1: "Очень надежная компания. Я получил быстрое и эффективное обслуживание.",
+        testimonal2: "С Ha&Fa Nord приехать в Германию было очень легко. Спасибо!",
+        terms: "Условия использования",
+        privacy: "Политика конфиденциальности",
+        edu_visa: "Учебная виза",
+        edu_visa_text: "Мы полностью поддерживаем вас в получении визы для обучения",
+        work_visa: "Рабочая виза",
+        work_visa_text: "Если вы хотите работать в Германии, мы можем предоставить вам как визовую поддержку, так и поддержку в трудоустройстве.",
+        aus: "Программа Ausbildung",
+        aus_text:"Если вы думаете начать новую карьеру в Германии, свяжитесь с нами.",
+        mission_text: "Наша миссия заключается в облегчении процесса для тех, кто хочет освоиться в Германии, и связывании их с подходящими возможностями занятости. Наш опытный коллектив предоставляет консультации и поддержку на каждом этапе, начиная от поиска работы и заканчивая оформлением виз, возможностями получения образования и этапами освоения.",
+        about_text: "Ha&Fa Nord - мы стоим за каждым человеком, предоставляя необходимую поддержку для успешной карьеры или образования в Германии.",
+        services_text: "Ha&Fa Nord - это компания, специализирующаяся на посредничестве при привлечении специалистов из-за рубежа для вакансий на немецком рынке труда. Мы предоставляем иностранцам ориентацию в сфере трудоустройства и помощь по прибытии в Германию. Помимо поддержки в поиске работы и получении визы для всех, кто хочет приехать в Германию, мы также помогаем тем, кто участвует в образовательных или повышающих квалификацию программах или желает пройти обучение, отвечая на вопросы по визам и школам."
     },
     De: {
         Azərbaycanca: "Azərbaycanca",
         Русский: "Русский",
-        Deutsch: "Deutsch!!!",
-        about: "Aboutde",
-        services: "Servicesde",
-        portfolio: "Portfoliode",
-        description: "This is a multilingual website template."
+        Deutsch: "Deutsch",
+        home: "Startseite",
+        about: "Über Uns",
+        services: "Unsere Services",
+        portfolio: "Portfolio",
+        team: "Unser Team",
+        contact: "Kontakt",
+        testimonals: "Kunden-Kommentare",
+        hero1: "Kompetente Unterstützung in",
+        hero2: "Geschäfts - und Visaangelegenheiten",
+        hero3: "um Ihren Ankunftsprozess in Deutschland zu erleichtern",
+        get_started_btn: "Mehr",
+        services_btn: "Unsere Services",
+        phone: "Telefon:",
+        email: "E-Adresse",
+        links: "Verknüpfung",
+        testimonal1: "Ein sehr zuverlässiges Unternehmen. Ich habe einen schnellen und effektiven Service erhalten.",
+        testimonal2: "Mit Ha&Fa Nord war es sehr einfach, nach Deutschland zu kommen. Danke!",
+        terms: "Servicebedingungen",
+        privacy: "Datenschutzrichtlinie",
+        edu_visa: "Visum zum Studium",
+        edu_visa_text: "Wir unterstützen Sie umfassend bei der Beantragung eines Visums für Ihr Studium",
+        work_visa: "Arbeitsvisum",
+        work_visa_text: "Wenn Sie in Deutschland arbeiten möchten, können wir Ihnen sowohl bei der Visums- als auch bei der Beschäftigungsunterstützung behilflich sein",
+        aus: "Ausbildung",
+        aus_text:"Wenn Sie darüber nachdenken, Ihre neue Karriere in Deutschland zu beginnen, kontaktieren Sie uns",
+        mission_text: "Unsere Mission konzentriert sich darauf, den Prozess für diejenigen zu erleichtern, die sich in Deutschland niederlassen möchten, und sie mit passenden Arbeitsmöglichkeiten zu verbinden. Unser erfahrenes Team bietet Beratung und Unterstützung in jedem Schritt, angefangen von der Arbeitssuche bis hin zu Visa-Anträgen, Bildungsmöglichkeiten und Siedlungsphasen.",
+        about_text: "Bei Ha&Fa Nord - stehen wir jedem Einzelnen zur Seite, um die erforderliche Unterstützung für eine erfolgreiche Karriere oder Bildung in Deutschland zu bieten.",
+        services_text: "Unser Ziel bei Ha&Fa Nord ist es, ausländischen Staatsangehörigen das Gefühl von Sicherheit bei der Jobsuche und Ausbildung in Deutschland zu vermitteln und ihnen dabei zu helfen, eine erfolgreiche Zukunft aufzubauen. Jeder einzelne hat bei uns das Potenzial für eine erfolgreiche Karriere, und wir streben danach, individuelle Lösungen anzubieten, um den Erfolg unserer Kandidaten zu fördern."
     }
 
     // Add more languages here
@@ -74,7 +119,7 @@ function changeLanguage(language) {
 
 document.addEventListener('DOMContentLoaded', event => {
     console.log(event);
-    changeLanguage("Az");
+    changeLanguage("De");
     console.log("lang change");
     });
 
